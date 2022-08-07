1b7c140f713fe9cb79436f61a6410fb1828e8121
KonstantinChukavin <KainkosYa@yandex.ru>
2022-08-07 16:00:02 +0300

    hot fix

--- task_4.js
+++ task_4.js
@@ -5,9 +5,9 @@
 const n = 0;                       // задаем значения диапазона
 const m = 100;
 
-let range = Math.abs(m - n);
+let range = Math.abs(m - n);               // приводим к абсолютному значению
 let numberInRange = Math.round(Math.random() * range);
-let min = Math.min(n, m);
+let min = Math.min(n, m);                  // находим минимальное значение
 let randomNumber = (min + numberInRange);  // это случайная переменная в заданном диапазоне
 
 console.log(randomNumber);                 // проверяем через консоль
